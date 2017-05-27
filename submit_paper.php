b27afe90acfb92bcabcae822a55e392d2f3454dc
Md. Foysal <foysal.iuice@gmail.com>
2017-05-27 17:58:22 +0600

    Delete old html code

--- submit_paper.php
+++ submit_paper.php
@@ -1,25 +1,24 @@
-<?php include("inc/header.php");?>
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-        <div class="panel panel-info">
-          <div class="panel-heading">Submit Paper</div>
-          <div class="panel-body">
-            <form action="" method="POST">
-              <div class="form-group">
-                <label for="exampleInputEmail1">Choose file</label>
-                <input type="file" name="att" class="form-control" required>
-              </div>
-              <button type="submit" class="btn btn-info">Submit</button>
-            </form>
-          </div>
-        </div>
-        </div>
-      </div>
-    </div>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="bootstrap/js/bootstrap.min.js"></script>
-  </body>
-</html>
+<?php include("inc/header.php"); ?>
+<?php
+ $login = Session::get("memberLogin");
+   if ($login==false) {
+    echo "<script>location.replace('login.php');</script>";
+   }
+?>
+<?php
+  if (isset($_GET['cid'])) {
+    Session::destroy();
+  }
+?>
+<a class="logout" href="?cid=<?php Session::get('m_Id'); ?>">Logout</a>
+  <p class="title">Submit</p> 
+  <form>
+    <div class="input-container">
+    <input type="file" name="att" required="required"/>
+    <div class="button-container">
+    <button name="login"><span>Submit</span></button>
+  </div>
+  </div>
+  </form>
+</div>
+<?php include('inc/footer.php');?>
