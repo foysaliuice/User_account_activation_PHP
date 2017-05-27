0f846400b28c0999ad7e823a977ed8331ca46dfd
Md. Foysal <foysal.iuice@gmail.com>
2017-05-27 17:55:58 +0600

    Add EmailActivation function

--- email_activation.php
+++ email_activation.php
@@ -1,3 +1,4 @@
+<?php include('inc/header.php');?>
 <?php include 'classes/MemberRegi.php'; ?>
 <?php
 	$member_regi = new MemberRegi();
@@ -7,19 +8,6 @@
 	$Update = $member_regi->EmailActivation($email, $code);
 
 ?>
-<!DOCTYPE html>
-<html >
-<head>
-  <meta charset="UTF-8">
-  <title>Member Portal</title>
-  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotoDraft:400,100,300,500,700,900'>
-  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
-  <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-  <div class="container">
-    <div class="card"></div>
-    <div class="card">
       <h1 class="title">Activated</h1>
       <div class="button-container">
       <?php
