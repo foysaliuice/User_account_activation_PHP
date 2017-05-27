94a3200cf6d09133b69d8c9b384f84aff8b5a510
Md. Foysal <foysal.iuice@gmail.com>
2017-05-27 10:08:53 +0600

    Include header and footer page

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+  <div class="footer-cl">
+  	<p>Design and Developed by Md. Foysal</p>
+  </div>
+  </div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src="js/index.js"></script>
+</body>
+</html>
