c5792a5fabea441c71e03baf013f9a1b8ad40804
Md. Foysal <foysal.iuice@gmail.com>
2017-05-27 17:57:05 +0600

    Delete header part from index

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html >
-<head>
-  <meta charset="UTF-8">
-  <title>Member Portal</title>
-  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900|RobotoDraft:400,100,300,500,700,900'>
-  <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
-  <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-  <div class="container">
-    <div class="card"></div>
-    <div class="card">
+<?php include('inc/header.php');?>
       <h1 class="title">Welcome</h1>
         <div class="button-container">
           <a href="login.php" class="button"><span>Login</span></a>
