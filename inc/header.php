9923b3feed9b408434d6a28cdad5b6ad643ec3d7
Md. Foysal <foysal.iuice@gmail.com>
2017-05-28 11:24:23 +0600

    Add new class contructor

--- inc/header.php
+++ inc/header.php
@@ -10,6 +10,7 @@ $db = new Database();
 $fm = new Format();
 $member_regi= new MemberRegi();
 $member_login = new MemberLogin();
+$submit_paper = new Paper();
 header("Cache-Control: no-cache, must-revalidate");
 header("Pragma: no-cache");
 header("Expires: Sat, 26 Jul 1997 05:00:00 GMT");
